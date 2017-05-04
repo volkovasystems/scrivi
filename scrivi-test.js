3e1a9bd58a7e40dd22559f4b38fd081b204ac315
johnlenon <johnlenonmaghanoy@gmail.com>
2017-05-04 18:32:46 +0800

    Updated test.

--- scrivi-test.js
+++ scrivi-test.js
@@ -1,8 +1,11 @@
+const assert = require( "assert" );
 const scrivi = require( "./scrivi.js" );
 
-console.log( scrivi( "./hello.world", "hello world", true ) )
+assert.equal( scrivi( "./hello.world", "hello world", true ), false, "should be false" );
 
 scrivi( "./hello.world", "yeah world" )
 	( function done( error, result ){
 		console.log( arguments );
 	} );
+
+console.log( "ok" );
