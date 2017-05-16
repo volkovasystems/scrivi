a5a6eb12c2d7f8dc5ac354b7de2e74af7e7c0b1c
johnlenon <johnlenonmaghanoy@gmail.com>
2017-05-16 12:17:38 +0800

    Updated test.

--- scrivi-test.js
+++ scrivi-test.js
@@ -1,7 +1,8 @@
 const assert = require( "assert" );
 const scrivi = require( "./scrivi.js" );
 
-assert.equal( scrivi( "./hello.world", "hello world", true ), false, "should be false" );
+assert.equal( scrivi( "./hello.world", "hello world", true ),
+false, "should be false" );
 
 scrivi( "./hello.world", "yeah world" )
 	( function done( error, result ){
